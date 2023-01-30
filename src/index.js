f40bfa0e438a472730d76cdeadd13dfacda50b0c
andraderika <oliandraderika@gmail.com>
2023-01-30 10:12:25 -0300

    versão atualizada html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const key = document.getElementById("key");
+const message01 = document.getElementById("message01");
+const btnEncode = document.getElementById("btnEncode");
+const btnDecode = document.getElementById("btnDecode");
+const message02 = document.getElementById("message02");
+
+btnEncode.addEventListener("click", acaoEncode);
+btnDecode.addEventListener("click", acaoDecode);
+
+function acaoEncode(){
+  message02.value = cipher.encode(parseInt(key.value), message01.value);
+}
+
+
+function acaoDecode(){
+  message02.value = cipher.decode(parseInt(key.value), message01.value);
+}
